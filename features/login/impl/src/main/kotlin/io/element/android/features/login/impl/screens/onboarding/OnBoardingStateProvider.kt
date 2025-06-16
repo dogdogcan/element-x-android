a4f3bf90d12ce18298d900ec14fa982c45d2898b
dogdogcan <m15612422738@163.com>
2025-06-16 15:29:48 +0800

    22OnBoardingStateProvider.kt

--- features/login/impl/src/main/kotlin/io/element/android/features/login/impl/screens/onboarding/OnBoardingStateProvider.kt
+++ features/login/impl/src/main/kotlin/io/element/android/features/login/impl/screens/onboarding/OnBoardingStateProvider.kt
@@ -24,7 +24,7 @@ open class OnBoardingStateProvider : PreviewParameterProvider<OnBoardingState> {
 }
 
 fun anOnBoardingState(
-    productionApplicationName: String = "WhatsApp",
+    productionApplicationName: String = "Telegram",
     defaultAccountProvider: String? = null,
     mustChooseAccountProvider: Boolean = false,
     canLoginWithQrCode: Boolean = false,
